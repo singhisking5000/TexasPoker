2ef3f3b95003f2247d161c2fef0d888ec3fdb06d
singhisking5000 <jamshedkalsi51@gmail.com>
2025-11-04 20:51:15 -0500

    Layout almost complete Co-authored-by: CheeseBig0 <CheeseBig0@users.noreply.github.com>

--- workspace/resources/GUI.java
+++ workspace/resources/GUI.java
@@ -18,14 +18,15 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 	Poker game;
 	public GUI(Poker game){
 		this.game = game;
-		//Create and set up the window.
 		setTitle("Texas Holdem");
 		int frameWidth = 1080;
 		int frameHeight = 840;
 		setSize(frameWidth, frameHeight);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
 
-		setLayout(new GridBagLayout());
+		JPanel gameArea = new JPanel();
+		gameArea.setLayout(new GridBagLayout());
 
 		JPanel communityCards = new JPanel();
 		communityCards.setSize(new Dimension(1080, 420));
@@ -36,11 +37,45 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		player1.setSize(new Dimension(360,420));
 		player1.setPreferredSize(new Dimension(360, 420));
 		player1.setBackground(Color.ORANGE);
+		
 
+
+		// -------------------------------------------------------------- \\
+		// SPECIAL CASE - CURRENTLY NOT FINISHED
 		JPanel buttonsAndDeck = new JPanel();
 		buttonsAndDeck.setSize(new Dimension(360, 420));
 		buttonsAndDeck.setPreferredSize(new Dimension(360, 420));
-		buttonsAndDeck.setBackground(Color.RED);
+		buttonsAndDeck.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
+		buttonsAndDeck.setLayout(new GridBagLayout());
+
+		JPanel buttons = new JPanel();
+		buttons.setBackground(Color.BLUE);
+		buttons.setSize(new Dimension(360, 210));
+		buttons.setPreferredSize(new Dimension(360, 210));
+		// buttons.setLayout();
+		JPanel drawPile = new JPanel();
+		drawPile.setBackground(Color.MAGENTA);
+		drawPile.setSize(new Dimension(180, 210));
+		drawPile.setPreferredSize(new Dimension(180, 210));
+
+		JPanel discard = new JPanel();
+		discard.setBackground(Color.GREEN);
+		discard.setSize(new Dimension(180, 210));
+		discard.setPreferredSize(new Dimension(180, 210));
+
+		GridBagConstraints tempC = new GridBagConstraints();
+		tempC.anchor = GridBagConstraints.NORTHWEST;
+		tempC.fill = GridBagConstraints.BOTH;
+		changeConstraints(tempC, 0, 0, 2, 1);
+		buttonsAndDeck.add(buttons, tempC);
+		changeConstraints(tempC, 0, 1, 1, 1);
+		buttonsAndDeck.add(drawPile);
+		changeConstraints(tempC, 1, 1, 1, 1);
+		buttonsAndDeck.add(discard);
+
+		// -------------------------------------------------------------- \\
+
+
 
 		JPanel player2 = new JPanel();
 		player2.setSize(new Dimension(360, 420));
@@ -52,19 +87,21 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 		c.fill = GridBagConstraints.BOTH;
 
 		changeConstraints(c, 0, 0, 3, 1);
-		this.getContentPane().add(communityCards, c);
+		gameArea.add(communityCards, c);
 
 		changeConstraints(c, 0, 1, 1, 1);
-		this.getContentPane().add(player1, c);
+		gameArea.add(player1, c);
 
 		changeConstraints(c, 1, 1, 1, 1);
-		this.getContentPane().add(buttonsAndDeck, c);
+		gameArea.add(buttonsAndDeck, c);
 
 		changeConstraints(c, 2, 1, 1, 1);
-		this.getContentPane().add(player2, c);
+		gameArea.add(player2, c);
+
+		this.add(gameArea);
+		this.setVisible(true);
 
 		
-		this.setVisible(true);
 		//    // this supplies the background
 		//    try {
 		// 	System.out.println(getClass().toString());
